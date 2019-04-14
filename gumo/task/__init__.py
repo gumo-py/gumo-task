2b477bed186c9c4dfc32dc76a23f4a5b082ba327
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-04-14 22:22:29 +0900

    Expose enqueue method in gumo.task

--- gumo/task/__init__.py
+++ gumo/task/__init__.py
@@ -2,12 +2,13 @@ from logging import getLogger
 
 from gumo.task._configuration import configure
 from gumo.task.domain.configuration import TaskConfiguration
+from gumo.task.application import enqueue
 
 
 __all__ = [
     configure.__name__,
-
     TaskConfiguration.__name__,
+    enqueue.__name__,
 ]
 
 logger = getLogger('gumo.task')
