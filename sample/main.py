dd7228886196bf1b61f21aa578e6118cf8ba22e7
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-06-28 21:52:24 +0900

    Sample Application of using gumo-task

--- sample/main.py
+++ sample/main.py
@@ -1,9 +1,28 @@
 import flask
 import logging
 import sys
+import os
+import datetime
+
+from gumo.core import configure as core_configure
+from gumo.task import configure as task_configure
+from gumo.task import enqueue
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+
+DEFAULT_QUEUE_NAME = 'gumo-task-test-queue'
+DELAYED_QUEUE_NAME = 'gumo-task-delayed-test-queue'
+
+core_configure(
+    google_cloud_project=os.environ['GOOGLE_CLOUD_PROJECT'],
+    google_cloud_location='us-central1',
+)
+
+task_configure(
+    default_queue_name=DEFAULT_QUEUE_NAME,
+    use_local_task_emulator=False,
+)
 
 app = flask.Flask(__name__)
 
@@ -11,6 +30,73 @@ app = flask.Flask(__name__)
 @app.route('/')
 def hello():
     return f'Hello, world. (gumo-task)'
+
+
+@app.route('/enqueue')
+def enqueue():
+    task1 = enqueue(
+        url='/enqueued-task',
+        method='POST',
+        payload={
+            'enqueued_at': datetime.datetime.utcnow().isoformat(),
+            'args': flask.request.args,
+        }
+    )
+
+    task2 = enqueue(
+        url='/delayed-task',
+        method='POST',
+        payload={
+            'enqueued_at': datetime.datetime.utcnow().isoformat(),
+            'args': flask.request.args,
+        },
+        in_seconds=3600,
+        queue_name=DELAYED_QUEUE_NAME
+    )
+
+    task3 = enqueue(
+        url='/failed-task',
+        method='GET',
+    )
+
+    text = f"""Task enqueued.
+    ----
+    task1 = {task1.key.key_literal()}
+    task2 = {task2.key.key_literal()}
+    task3 = {task3.key.key_literal()}
+    ----
+    {task1}
+    ----
+    {task2}
+    ----
+    {task3}
+    """
+
+    return flask.Response(text, content_type='text/plain')
+
+
+@app.route('/enqueued-task')
+def enqueued_task():
+    logger.info(f'enqueued-task called.')
+    logger.info(f'flask.request.args = {flask.request.args}')
+
+    return 'ok'
+
+
+@app.route('/delayed-task')
+def delayed_task():
+    logger.info(f'delayed-task called.')
+    logger.info(f'flask.request.args = {flask.request.args}')
+
+    return 'ok'
+
+
+@app.route('/failed-task')
+def failed_task():
+    logger.info(f'failed-task called.')
+    logger.info(f'flask.request.args = {flask.request.args}')
+
+    return flask.Response('failed', status=500)
 
 
 if __name__ == '__main__':
