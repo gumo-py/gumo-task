5e1d15f27e9dd015c8e662f1ab25121a72070b83
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-06-29 13:47:47 +0900

    Remove unused parameter

--- gumo/task/domain/configuration/__init__.py
+++ gumo/task/domain/configuration/__init__.py
@@ -1,7 +1,9 @@
 import dataclasses
 
+from typing import Optional
+
 
 @dataclasses.dataclass(frozen=True)
 class TaskConfiguration:
-    default_queue_name: str
+    default_queue_name: Optional[str] = None
     use_local_task_emulator: bool = False
