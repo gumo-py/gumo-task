c4a5a48721a874c8b9f4e9458d7d41e25aad2bad
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-07-09 23:23:03 +0900

    Rename constants: add prefix "_ENV_KEY"

--- gumo/task/infrastructure/configuration/__init__.py
+++ gumo/task/infrastructure/configuration/__init__.py
@@ -95,9 +95,9 @@ class TaskConfiguration:
     cloud_tasks_location: Optional[CloudTaskLocation] = None
     client: Optional[tasks.CloudTasksClient] = None
 
-    _GOOGLE_CLOUD_PROJECT_ENV_KEY: ClassVar = 'GOOGLE_CLOUD_PROJECT'
-    _GAE_SERVICE_ENV_KEY: ClassVar = 'GAE_SERVICE'
-    _FALLBACK_CLOUD_TASKS_LOCATION: ClassVar = '_FALLBACK_CLOUD_TASKS_LOCATION'
+    _ENV_KEY_GOOGLE_CLOUD_PROJECT: ClassVar = 'GOOGLE_CLOUD_PROJECT'
+    _ENV_KEY_GAE_SERVICE: ClassVar = 'GAE_SERVICE'
+    _ENV_KEY_FALLBACK_CLOUD_TASKS_LOCATION: ClassVar = '_FALLBACK_CLOUD_TASKS_LOCATION'
 
     _lock: ClassVar = threading.Lock()
 
@@ -109,22 +109,22 @@ class TaskConfiguration:
             self._set_client()
 
     def _has_fallback_location(self) -> bool:
-        return self._FALLBACK_CLOUD_TASKS_LOCATION in os.environ
+        return self._ENV_KEY_FALLBACK_CLOUD_TASKS_LOCATION in os.environ
 
     def _set_google_cloud_project(self):
         if isinstance(self.google_cloud_project, str):
             self.google_cloud_project = GoogleCloudProjectID(self.google_cloud_project)
         if isinstance(self.google_cloud_project, GoogleCloudProjectID):
-            if self.google_cloud_project.value != os.environ.get(self._GOOGLE_CLOUD_PROJECT_ENV_KEY):
-                raise RuntimeError(f'Env-var "{self._GOOGLE_CLOUD_PROJECT_ENV_KEY}" is invalid or undefined.'
+            if self.google_cloud_project.value != os.environ.get(self._ENV_KEY_GOOGLE_CLOUD_PROJECT):
+                raise RuntimeError(f'Env-var "{self._ENV_KEY_GOOGLE_CLOUD_PROJECT}" is invalid or undefined.'
                                    f'Please set value "{self.google_cloud_project.value}" to env-vars.')
 
-        if self.google_cloud_project is None and self._GOOGLE_CLOUD_PROJECT_ENV_KEY in os.environ:
-            self.google_cloud_project = GoogleCloudProjectID(os.environ[self._GOOGLE_CLOUD_PROJECT_ENV_KEY])
+        if self.google_cloud_project is None and self._ENV_KEY_GOOGLE_CLOUD_PROJECT in os.environ:
+            self.google_cloud_project = GoogleCloudProjectID(os.environ[self._ENV_KEY_GOOGLE_CLOUD_PROJECT])
 
     def _set_gae_service_name(self):
-        if self.gae_service_name is None and self._GAE_SERVICE_ENV_KEY in os.environ:
-            self.gae_service_name = os.environ[self._GAE_SERVICE_ENV_KEY]
+        if self.gae_service_name is None and self._ENV_KEY_GAE_SERVICE in os.environ:
+            self.gae_service_name = os.environ[self._ENV_KEY_GAE_SERVICE]
 
     def _set_cloud_tasks_location(self):
         if isinstance(self.cloud_tasks_location, CloudTaskLocation):
@@ -135,8 +135,10 @@ class TaskConfiguration:
             return
 
         if self._has_fallback_location():
-            location_id = os.environ[self._FALLBACK_CLOUD_TASKS_LOCATION]
-            logger.debug(f'Fallback to location={location_id} via env-vars "{self._FALLBACK_CLOUD_TASKS_LOCATION}"')
+            location_id = os.environ[self._ENV_KEY_FALLBACK_CLOUD_TASKS_LOCATION]
+            logger.debug(
+                f'Fallback to location={location_id} via env-vars "{self._ENV_KEY_FALLBACK_CLOUD_TASKS_LOCATION}"'
+            )
             self.cloud_tasks_location = CloudTaskLocation.build_by(
                 project_id=self.google_cloud_project.value,
                 location_id=location_id,
