1d841c67129a4bcf289ac59fa0b5b1a3e69a6d1f
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-11-15 16:53:21 +0900

    Refactoring GumoTaskMapper

--- gumo/task/infrastructure/repository/__init__.py
+++ gumo/task/infrastructure/repository/__init__.py
@@ -68,7 +68,5 @@ class GumoTaskRepositoryImpl(GumoTaskRepository, DatastoreRepositoryMixin):
             queue_name: Optional[str] = None
     ):
         logger.debug(f'Use Tasks Local Emulator with Datastore (task={task}, queue_name={queue_name})')
-        datastore_key = self.entity_key_mapper.to_datastore_key(entity_key=task.key)
-        datastore_entity = self.DatastoreEntity(key=datastore_key)
-        datastore_entity.update(self._task_mapper.to_datastore_entity(task))
+        datastore_entity = self._task_mapper.to_datastore_entity(task=task)
         self.datastore_client.put(datastore_entity)
--- gumo/task/infrastructure/repository/mapper.py
+++ gumo/task/infrastructure/repository/mapper.py
@@ -1,22 +1,24 @@
-from gumo.core import EntityKey
 from gumo.task.domain import GumoTask
 from gumo.datastore.infrastructure import DatastoreMapperMixin
+from gumo.datastore.infrastructure import DatastoreEntity
 
 
 class DatastoreGumoTaskMapper(DatastoreMapperMixin):
-    def to_datastore_entity(self, task: GumoTask) -> dict:
-        j = {
+    def to_datastore_entity(self, task: GumoTask) -> DatastoreEntity:
+        doc = DatastoreEntity(key=self.entity_key_mapper.to_datastore_key(task.key))
+        doc.update({
             'relative_uri': task.relative_uri,
             'method': task.method,
             'payload': task.payload,
             'schedule_time': task.schedule_time,
             'created_at': task.created_at,
             'queue_name': task.queue_name,
-        }
+        })
 
-        return j
+        return doc
 
-    def to_entity(self, key: EntityKey, doc: dict) -> GumoTask:
+    def to_entity(self, doc: DatastoreEntity) -> GumoTask:
+        key = self.entity_key_mapper.to_entity_key(doc.key)
         return GumoTask(
             key=key,
             relative_uri=doc.get('relative_uri', doc.get('url')),
