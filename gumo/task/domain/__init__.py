21b142dad3c85efd1d4df9473fe40694aa2e1295
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-11-15 16:59:10 +0900

    Supports routing configuration for each task

--- gumo/task/domain/__init__.py
+++ gumo/task/domain/__init__.py
@@ -7,6 +7,13 @@ from gumo.core import EntityKey
 
 
 @dataclasses.dataclass(frozen=True)
+class TaskAppEngineRouting:
+    service: Optional[str] = None
+    version: Optional[str] = None
+    instance: Optional[str] = None
+
+
+@dataclasses.dataclass(frozen=True)
 class GumoTask:
     KIND = 'GumoTask'
 
@@ -17,12 +24,13 @@ class GumoTask:
     schedule_time: datetime.datetime = dataclasses.field(default_factory=datetime.datetime.utcnow)
     created_at: datetime.datetime = dataclasses.field(default_factory=datetime.datetime.utcnow)
     queue_name: Optional[str] = None
+    app_engine_routing: Optional[TaskAppEngineRouting] = None
 
-    def _clone(self, **changes):
-        """
-        :rtype: GumoTask
-        """
+    def _clone(self, **changes) -> "GumoTask":
         return dataclasses.replace(self, **changes)
 
-    def with_queue_name(self, queue_name: str):
+    def with_queue_name(self, queue_name: str) -> "GumoTask":
         return self._clone(queue_name=queue_name)
+
+    def with_routing(self, routing: TaskAppEngineRouting) -> "GumoTask":
+        return self._clone(app_engine_routing=routing)
--- gumo/task/infrastructure/repository/mapper.py
+++ gumo/task/infrastructure/repository/mapper.py
@@ -1,4 +1,5 @@
 from gumo.task.domain import GumoTask
+from gumo.task.domain import TaskAppEngineRouting
 from gumo.datastore.infrastructure import DatastoreMapperMixin
 from gumo.datastore.infrastructure import DatastoreEntity
 
@@ -6,6 +7,15 @@ from gumo.datastore.infrastructure import DatastoreEntity
 class DatastoreGumoTaskMapper(DatastoreMapperMixin):
     def to_datastore_entity(self, task: GumoTask) -> DatastoreEntity:
         doc = DatastoreEntity(key=self.entity_key_mapper.to_datastore_key(task.key))
+
+        routing = {}
+        if task.app_engine_routing:
+            routing = {
+                'service': task.app_engine_routing.service,
+                'version': task.app_engine_routing.version,
+                'instance': task.app_engine_routing.instance,
+            }
+
         doc.update({
             'relative_uri': task.relative_uri,
             'method': task.method,
@@ -13,12 +23,22 @@ class DatastoreGumoTaskMapper(DatastoreMapperMixin):
             'schedule_time': task.schedule_time,
             'created_at': task.created_at,
             'queue_name': task.queue_name,
+            'app_engine_routing': routing,
         })
 
         return doc
 
     def to_entity(self, doc: DatastoreEntity) -> GumoTask:
         key = self.entity_key_mapper.to_entity_key(doc.key)
+
+        routing = None
+        if 'app_engine_routing' in doc:
+            routing = TaskAppEngineRouting(
+                service=doc['app_engine_routing'].get('service'),
+                version=doc['app_engine_routing'].get('version'),
+                instance=doc['app_engine_routing'].get('instance'),
+            )
+
         return GumoTask(
             key=key,
             relative_uri=doc.get('relative_uri', doc.get('url')),
@@ -27,4 +47,5 @@ class DatastoreGumoTaskMapper(DatastoreMapperMixin):
             schedule_time=self.convert_datetime(doc.get('schedule_time')),
             created_at=self.convert_datetime(doc.get('created_at')),
             queue_name=doc.get('queue_name'),
+            app_engine_routing=routing,
         )
